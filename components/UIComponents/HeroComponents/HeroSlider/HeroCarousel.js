f9a44fee81fba2482c8a0739e67848348b4fe923
Muratcan Baba <muratcanbaba@Muratcan-MacBook-Pro.local>
2023-02-17 16:25:32 +0300

    feature: buttons carousel

--- components/UIComponents/HeroComponents/HeroSlider/HeroCarousel.js
+++ components/UIComponents/HeroComponents/HeroSlider/HeroCarousel.js
@@ -1,25 +1,42 @@
-import React from "react";
-import { Carousel, Col, Row } from "antd";
+import React, { useRef } from "react";
+import { Button, Carousel, Col, Row } from "antd";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@ant-design/icons";
 import { SectorCard } from "./SectorCard";
 import { data } from "@/data/sectors.js";
 
-const HeroCarousel = () => (
-  <Carousel>
-    <Row>
-      {data.map(item => (
-        <Col key={item.id} xs={24} lg={8}>
-          <SectorCard features={item.features} title={item.title} icon={item.icon} description={item.description} />
-        </Col>
-      ))}
-    </Row>
-    <Row>
-      {data.map(item => (
-        <Col key={item.id} xs={24} lg={8}>
-          <SectorCard features={item.features} title={item.title} icon={item.icon} description={item.description} />
-        </Col>
-      ))}
-    </Row>
-  </Carousel>
-);
+const HeroCarousel = () => {
+
+  const
+    carouselRef = useRef(null),
+    handleNext = () => carouselRef.current.next(),
+    handlePrev = () => carouselRef.current.prev();
+
+  return (
+    <div className="carousel-container">
+      <Button onClick={handlePrev} type="text" className="carousel-slider-button" >
+        <ArrowLeftOutlined className="carousel-slider-button-icon" />
+      </Button>
+      <Carousel ref={carouselRef} dotPosition="none">
+        <Row>
+          {data.first.map(item => (
+            <Col key={item.id} xs={24} lg={8}>
+              <SectorCard features={item.features} title={item.title} icon={item.icon} description={item.description} />
+            </Col>
+          ))}
+        </Row>
+        <Row>
+          {data.second.map(item => (
+            <Col key={item.id} xs={24} lg={8}>
+              <SectorCard features={item.features} title={item.title} icon={item.icon} description={item.description} />
+            </Col>
+          ))}
+        </Row>
+      </Carousel>
+      <Button onClick={handleNext} type="text" className="carousel-slider-button">
+        <ArrowRightOutlined className="carousel-slider-button-icon" />
+      </Button>
+    </div>
+  );
+};
 
 export default HeroCarousel;
--- data/sectors.js
+++ data/sectors.js
@@ -1,41 +1,57 @@
 import TourismSvg from "@/assets/images/sectors/tourism.svg";
 import ECommerceSvg from "@/assets/images/sectors/e-commerce.svg";
 import SaasSvg from "@/assets/images/sectors/saas.svg";
-export const data = [
-  {
-    id: 1,
-    title: "label.tourism",
-    description: "message.tourismDescription",
-    features: [
-      "label.walletService",
-      "label.physcorVirtCar",
-      "label.physcorVirtPos",
-      "label.fraudMonitoring"
-    ],
-    icon: <TourismSvg />
-  },
-  {
-    id: 2,
-    title: "label.saas",
-    description: "message.saasDescription",
-    features: [
-      "label.physcorVirtCar",
-      "label.qrPayment",
-      "label.campaignManagement",
-      "label.3dSecurePayment"
-    ],
-    icon: <SaasSvg />
-  },
-  {
-    id: 3,
-    title: "label.ecommerce",
-    description: "message.ecommerceDescription",
-    features: [
-      "label.virtualPos",
-      "label.walletService",
-      "label.bnplCollectionSolution",
-      "label.customerRating"
-    ],
-    icon: <ECommerceSvg />
-  }
-];
+export const data = {
+  first: [
+    {
+      id: 1,
+      title: "label.tourism",
+      description: "message.tourismDescription",
+      features: [
+        "label.walletService",
+        "label.physcorVirtCar",
+        "label.physcorVirtPos",
+        "label.fraudMonitoring"
+      ],
+      icon: <TourismSvg />
+    },
+    {
+      id: 2,
+      title: "label.saas",
+      description: "message.saasDescription",
+      features: [
+        "label.physcorVirtCar",
+        "label.qrPayment",
+        "label.campaignManagement",
+        "label.3dSecurePayment"
+      ],
+      icon: <SaasSvg />
+    }
+  ],
+  second: [
+    {
+      id: 3,
+      title: "label.ecommerce",
+      description: "message.ecommerceDescription",
+      features: [
+        "label.virtualPos",
+        "label.walletService",
+        "label.bnplCollectionSolution",
+        "label.customerRating"
+      ],
+      icon: <ECommerceSvg />
+    },
+    {
+      id: 2,
+      title: "label.saas",
+      description: "message.saasDescription",
+      features: [
+        "label.physcorVirtCar",
+        "label.qrPayment",
+        "label.campaignManagement",
+        "label.3dSecurePayment"
+      ],
+      icon: <SaasSvg />
+    }
+  ]
+};
